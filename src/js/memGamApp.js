725e7462f1cd4c6d1ac1a5482271ccc178fcb78d
Christiano0407 <placebo0704@gmail.com>
2022-03-06 14:09:00 -0600

    add all list products

--- src/js/memGamApp.js
+++ src/js/memGamApp.js
@@ -1,1 +1,41 @@
 // Memory Game >>
+//> Const ==>
+const result = document.getElementById("result");
+const grid = document.getElementById("grid");
+//> Images =>
+const cardArray = [
+  {
+    name: "fries",
+    img: "../src/assets/memory02/fries.png",
+  },
+  {
+    name: "blank",
+    img: "../src/assets/memory02/blank.png",
+  },
+  {
+    name: "cheeseburger",
+    img: "../src/assets/memory02/cheeseburger.png",
+  },
+  {
+    name: "hotdog",
+    img: "../src/assets/memory02/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    img: "../src/assets/memory02/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "../src/assets/memory02/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "../src/assets/memory02/pizza.png",
+  },
+  {
+    name: "white",
+    img: "../src/assets/memory02/white.png",
+  },
+];
+console.log(cardArray);
+//> Events ==>
