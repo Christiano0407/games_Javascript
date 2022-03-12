49a56cb691ba50523ac8c4343137633fa78a83cc
Christiano0407 <placebo0704@gmail.com>
2022-03-12 11:45:02 -0600

    project games javascript 02

--- src/js/memGamApp.js
+++ src/js/memGamApp.js
@@ -36,8 +36,46 @@ const cardArray = [
     name: "white",
     img: "../src/assets/memory02/white.png",
   },
+  {
+    name: "pizza",
+    img: "../src/assets/memory02/pizza.png",
+  },
+  {
+    name: "hotdog",
+    img: "../src/assets/memory02/hotdog.png",
+  },
+  {
+    name: "fries",
+    img: "../src/assets/memory02/fries.png",
+  },
+  {
+    name: "ice-cream",
+    img: "../src/assets/memory02/ice-cream.png",
+  },
 ];
 //> Events ==>
 //> sort ordenado >
 cardArray.sort(() => 0.5 - Math.random());
-console.log(cardArray);
+/* console.log(cardArray); */
+
+//> Crear e llamar a las img.
+const gridDisplay = document.getElementById("grid");
+console.log(gridDisplay);
+
+createBoard = () => {
+  for (let i = 0; i < cardArray.length; i++) {
+    const card = document.createElement("img");
+    card.setAttribute("src", "../assets/memory02/blank.png");
+    card.setAttribute("data-id", i);
+    card.addEventListener("click", flipCard);
+    gridDisplay.appendChild(card);
+  }
+};
+
+createBoard();
+
+// Flip => MEMORY O Intersection ==> === >
+function flipCard() {
+  const cardId = this.getAttribute("data-id");
+  console.log("clicked", cardId);
+}
